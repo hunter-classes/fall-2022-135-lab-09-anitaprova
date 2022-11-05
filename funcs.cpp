f20d3dcb4a2598bf40d787751d30dda928a5202c
anitaprova <104943181+anitaprova@users.noreply.github.com>
2022-11-05 19:29:50 -0400

    added TaskA

--- funcs.cpp
+++ funcs.cpp
@@ -1,5 +1,11 @@
 #include <iostream>
+#include <cmath>
 #include "funcs.h"
+#include "coord3d.h"
 
 // add functions here
-
+double length(Coord3D *p) {	
+	double d = pow((*p).x,2) + pow((*p).y,2) + pow((*p).z,2);	
+	
+	return sqrt(d);
+}
--- main.cpp
+++ main.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
-
-
 #include "funcs.h"
+#include "coord3d.h"
 
 int main()
 {
-  return 0;
+	Coord3D pointP = {10, 20, 30};
+	std::cout << length(&pointP) << std::endl;
+	
+	return 0;
 }
