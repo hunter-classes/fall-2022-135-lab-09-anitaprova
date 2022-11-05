a6961f879a3b9ab085464c76a12e90a64bd70d87
anitaprova <104943181+anitaprova@users.noreply.github.com>
2022-11-05 19:41:34 -0400

    adding TaskB

--- funcs.cpp
+++ funcs.cpp
@@ -3,9 +3,20 @@
 #include "funcs.h"
 #include "coord3d.h"
 
-// add functions here
 double length(Coord3D *p) {	
 	double d = pow((*p).x,2) + pow((*p).y,2) + pow((*p).z,2);	
 	
 	return sqrt(d);
+}
+
+Coord3D* fartherFromOrigin(Coord3D *p1, Coord3D *p2){
+	double p1_length = length(p1);
+	double p2_length = length(p2);	
+	
+	if (p1_length > p2_length) {
+		return p1;	
+	}
+	else {
+		return p2;
+	}
 }
--- main.cpp
+++ main.cpp
@@ -5,7 +5,21 @@
 int main()
 {
 	Coord3D pointP = {10, 20, 30};
+	Coord3D pointQ = {-20, 21, -22};
+	
+	//TaskA
 	std::cout << length(&pointP) << std::endl;
+	std::cout << length(&pointQ) << std::endl;
+	
+	//TaskB
+	std::cout << "Address of P: " << &pointP << std::endl;
+	std::cout << "Address of Q: " << &pointQ << std::endl << std::endl;
+
+    	Coord3D * ans = fartherFromOrigin(&pointP, &pointQ);
+
+	std::cout << "ans = " << ans << std::endl;
+	
+	
 	
 	return 0;
 }
